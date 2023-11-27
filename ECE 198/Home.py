185db3ec9026aba93180d557a4450473961f3fc1
zen2504 <150866612+zen2504@users.noreply.github.com>
2023-11-27 01:38:34 -0500

    Add files via upload

--- ECE 198/Home.py
+++ ECE 198/Home.py
@@ -5,9 +5,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import subprocess
 
-# Replace these with your actual Spotify API credentials
-SPOTIPY_CLIENT_ID ='7158b9f6b992465fa597983e18e173d2'
-SPOTIPY_CLIENT_SECRET = '09b691ffbabd404a92cf6f1e06c01c57'
+#Credentials
+SPOTIPY_CLIENT_ID ='c6ed1a7766654ebd90ca61fb3b0221dc'
+SPOTIPY_CLIENT_SECRET = '8720b015b6a24e8eacbced79b1f5ba68'
 
 class SpotifyApp:
     def __init__(self, root):
@@ -17,7 +17,7 @@ class SpotifyApp:
         # Dictionary to store album objects and their tracks
         self.albums = {}
 
-        # Spotify color scheme
+        #Color scheme
         self.spotify_green = "#1DB954"
         self.spotify_black = "#191414"
         self.spotify_white = "#FFFFFF"
@@ -29,7 +29,7 @@ class SpotifyApp:
         self.style.configure("Treeview", background=self.spotify_white, fieldbackground=self.spotify_white)
         self.style.map("Treeview", background=[("selected", self.spotify_green)])
 
-        # Configure root window background color
+        #Root window background color
         root.configure(bg=self.spotify_black)
 
         # Create and place widgets for searching
@@ -45,86 +45,86 @@ class SpotifyApp:
         self.search_button = ttk.Button(self.search_frame, text="Search", command=self.search_album, style="TButton")
         self.search_button.grid(row=0, column=2, padx=5)
 
-        # Create canvas
+        #Canvas
         self.canvas = tk.Canvas(root, width=700, height=150, background=self.spotify_black)  # Increased dimensions
         self.canvas.pack(side=tk.TOP, padx=10, pady=10)
 
-       # Load the Spotify logo using tkinter.PhotoImage and resize it
+       #Load and Resize the Spotify Logo
         image_path = "spotify_logo.png"  # Replace with the actual filename of your downloaded image
         spotify_logo = tk.PhotoImage(file=image_path)
         spotify_logo = spotify_logo.subsample(8, 8)  # Adjust the subsample values to resize the image
 
-        # Create a Label to display the resized Spotify logo
+        #Create a Label to display the resized Spotify logo
         logo_label = ttk.Label(root, image=spotify_logo, background=self.spotify_black)
         logo_label.photo = spotify_logo  # Keep a reference to the image to prevent garbage collection
         logo_label.pack(side=tk.LEFT, padx=10, pady=10)
 
-        # Create a Frame for the album history
+        #Create a Frame for the album history
         self.frame = ttk.Frame(root)
         self.frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-        # Create a Treeview for the album history
+        #Create a Treeview for the album history
         self.album_tree = ttk.Treeview(self.frame, columns=("Album", "Artist"), height=10, show="headings")
         self.album_tree.heading("Album", text="Album")
         self.album_tree.heading("Artist", text="Artist")
         self.album_tree.pack(side=tk.LEFT, padx=10)
 
-        # Create a vertical scrollbar for the Treeview
+        #Create a scrollbar for the Treeview
         y_scrollbar = ttk.Scrollbar(self.frame, orient="vertical", command=self.album_tree.yview)
         y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.album_tree.configure(yscroll=y_scrollbar.set)
 
-        # Button to clear the album list
+        #Creates Button to clear the album list
         self.clear_button = ttk.Button(root, text="Clear List", command=self.clear_albums, style="TButton")
         self.clear_button.pack(side=tk.LEFT, pady=10)
 
-        # Bind the function to load new songs when an item in the Treeview is selected
+        #Bind the function to load new songs when an item in the Treeview is selected
         self.album_tree.bind("<<TreeviewSelect>>", self.load_new_songs)
 
     def search_album(self):
         album_name = self.album_entry.get()
 
-        # Search for the album
+        #Search for album
         results = self.sp.search(q=album_name, type='album', limit=1)
 
-        # Extract the album URI
+        #Extract the album URI
         if results['albums']['items']:
             album_uri = results['albums']['items'][0]['uri']
 
-            # Get all tracks for the album
+            #Get all tracks from the album
             all_tracks = self.sp.album_tracks(album_uri)['items']
 
-            # Randomly select 5 songs from the album
+            #Randomly select 5 songs from the album
             random_tracks = random.sample(all_tracks, min(5, len(all_tracks)))
 
-            # Display the results on the canvas
+            #Display the results on the canvas
             self.canvas.delete("result_text")  # Clear previous results
             for i, track in enumerate(random_tracks):
                 text = f"{i + 1}. {track['name']} by {', '.join([artist['name'] for artist in track['artists']])}"
                 self.canvas.create_text(10, 10 + i * 30, anchor="w", text=text, font=("Arial", 12), fill=self.spotify_white, tags="result_text")
 
-            # Add the album to the dictionary
+            #Add the album to the dictionary
             self.albums[album_name] = all_tracks
 
-            # Add the album to the Treeview
+            #Add the album to the Treeview
             self.album_tree.insert("", "end", values=(album_name, ', '.join([artist['name'] for artist in all_tracks[0]['artists']])))
 
     def load_new_songs(self, event):
-        # Get the selected item in the Treeview
+        #Get the selected item in the Treeview
         selected_item = self.album_tree.selection()
 
         if selected_item:
-            # Get the album name
+            #Get the album name
             album_name = self.album_tree.item(selected_item, "values")[0]
 
-            # Get all tracks for the selected album
+            #Get all tracks for the selected album
             all_tracks = self.albums.get(album_name, [])
 
-            # Randomly select 5 songs from the album
+            #Randomly select 5 songs from the album
             random_tracks = random.sample(all_tracks, min(5, len(all_tracks)))
 
-            # Display the results on the canvas
+            #Display the results on the canvas
             self.canvas.delete("result_text")  # Clear previous results
             for i, track in enumerate(random_tracks):
                 text = f"{i + 1}. {track['name']} by {', '.join([artist['name'] for artist in track['artists']])}"
@@ -136,15 +136,15 @@ class SpotifyApp:
         self.canvas.delete("result_text")
 
 if __name__ == "__main__":
-    # Run the installing.py script
+    #Run the installing.py script
     subprocess.call(["python", "installing.py"])
 
-    # Create the Tkinter root window
+    #Create the Tkinter root window
     root = tk.Tk()
     root.title("Spotify Top 5 Search")
 
-    # Create the SpotifyApp instance
+    #Create the SpotifyApp instance
     app = SpotifyApp(root)
 
-    # Start the Tkinter event loop
+    #Start the Tkinter event loop
     root.mainloop()
